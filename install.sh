424b5685ca077dbbafe32e96c435c3a2749bf75e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-05-17 12:37:32 -0400

    install package

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 PATH_MILLEGRILLES=/var/opt/millegrilles
 PATH_VENV=/var/opt/millegrilles/venv
+MG_PIP_REPOSITORY_URL=https://pip.maceroc.com/
+
+PACKAGE_MESSAGES=dist/millegrilles.messages-2022.3.0.tar.gz
 
 echo "Installer packages apt"
 sudo apt install -y python3-pip python3-venv
@@ -12,3 +15,6 @@ if [ ! -d "$PATH_VENV" ]; then
 fi
 
 echo "Installer pip requirements.txt"
+pip3 install wheel
+
+pip3 install "$MG_PIP_REPOSITORY_URL/$PACKAGE_MESSAGES"
