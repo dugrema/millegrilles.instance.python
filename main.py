e9d18d4a48b57eb2d8da8ed5c07a57ed5d068149
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-05-17 10:17:01 -0400

    Base pour application instance

--- main.py
+++ main.py
@@ -1,16 +1,125 @@
-# This is a sample Python script.
+# Main script d'application pour une instance MilleGrilles
+# L'application gere les secrets, certificats et site web de configuration (port 8080).
+import argparse
+import logging
+import signal
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from threading import Thread, Event
 
 
-# Press the green button in the gutter to run the script.
+def initialiser_application():
+    logging.basicConfig()
+
+    app = ApplicationInstance()
+
+    args = parse()
+    if args.verbose:
+        logging.getLogger(__name__).setLevel(logging.DEBUG)
+        logging.getLogger('millegrilles').setLevel(logging.DEBUG)
+    else:
+        logging.getLogger(__name__).setLevel(logging.INFO)
+        logging.getLogger('millegrilles').setLevel(logging.WARN)
+
+    app.charger_configuration(args)
+
+    signal.signal(signal.SIGINT, app.exit_gracefully)
+    signal.signal(signal.SIGTERM, app.exit_gracefully)
+    signal.signal(signal.SIGHUP, app.reload)
+
+    app.preparer_environnement()
+
+    return app
+
+
+def parse():
+    logger = logging.getLogger(__name__ + '.parse')
+    parser = argparse.ArgumentParser(description="Demarrer l'application d'instance MilleGrilles")
+
+    parser.add_argument(
+        '--verbose', action="store_true", required=False,
+        help="Active le logging maximal"
+    )
+
+    args = parser.parse_args()
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+
+    logger.debug("args : %s" % args)
+
+    return args
+
+
+class ApplicationInstance:
+
+    def __init__(self):
+        self.__logger = logging.getLogger(__name__ + '.' + self.__class__.__name__)
+        self._stop_event = Event()  # Evenement d'arret global de l'application
+
+    def charger_configuration(self, args: argparse.Namespace):
+        """
+        Charge la configuration (os.env, /var/opt/millegrilles/instance)
+        :return:
+        """
+        self.__logger.info("Charger la configuration")
+
+    def preparer_environnement(self):
+        """
+        Examine environnement, preparer au besoin (folders, docker, ports, etc)
+        :return:
+        """
+        self.__logger.info("Preparer l'environnement")
+
+    def reload(self):
+        self.__logger.info("Reload configuration")
+
+    def exit_gracefully(self, signum=None, frame=None):
+        self.__logger.info("Fermer application, signal: %d" % signum)
+        self.fermer()
+
+    def entretien(self):
+        """
+        Entretien du systeme. Invoque a intervalle regulier.
+        :return:
+        """
+        self.__logger.debug("Debut cycle d'entretien")
+
+        self.__logger.debug("Fin cycle d'entretien")
+
+    def fermer(self):
+        self._stop_event.set()
+
+    def executer(self):
+        """
+        Boucle d'execution principale
+        :return:
+        """
+        while not self._stop_event.is_set():
+
+            # Entretien
+            self.entretien()
+
+            self._stop_event.wait(2)  # Attente 30 secondes entre entretien
+
+
+def main():
+    """
+    Methode d'execution de l'application
+    :return:
+    """
+    app = initialiser_application()
+    logger = logging.getLogger(__name__)
+
+    try:
+        logger.info("Debut execution app")
+        app.executer()
+        logger.info("Fin execution app")
+    except KeyboardInterrupt:
+        logger.info("Arret execution app via signal (KeyboardInterrupt), fin thread main")
+    except:
+        logger.exception("Exception durant execution app, fin thread main")
+    finally:
+        app.fermer()  # S'assurer de mettre le flag de stop_event
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
