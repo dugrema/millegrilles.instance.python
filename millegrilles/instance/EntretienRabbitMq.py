f17c02576f30a3570232f84b148cfe8a5fc96284
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-05-26 19:06:17 -0400

    Creation compte mq admin, suppression guest

--- millegrilles/instance/EntretienRabbitMq.py
+++ millegrilles/instance/EntretienRabbitMq.py
@@ -5,6 +5,7 @@ import ssl
 
 from typing import Optional
 
+from aiohttp.client_exceptions import ClientConnectorError
 from asyncio import Event, TimeoutError
 
 
@@ -20,6 +21,8 @@ class EntretienRabbitMq:
         ca_path = etat_instance.configuration.instance_ca_pem_path
         self.__sslcontext = ssl.create_default_context(cafile=ca_path)
 
+        self.__entretien_initial_complete = False
+
     async def entretien(self):
         self.__logger.debug("entretien debut")
 
@@ -32,13 +35,23 @@ class EntretienRabbitMq:
                     self.__session = aiohttp.ClientSession(auth=basic_auth)
 
             if self.__session is not None:
-                async with self.__session.get('https://127.0.0.1:8443/api/health/checks/alarms', ssl=self.__sslcontext) as reponse:
-                    pass
-                self.__logger.debug("Reponse MQ : %s" % reponse)
+                try:
+                    async with self.__session.get('https://127.0.0.1:8443/api/health/checks/alarms', ssl=self.__sslcontext) as reponse:
+                        pass
+                    self.__logger.debug("Reponse MQ : %s" % reponse)
 
-                if reponse.status == 401:
-                    self.__logger.warning("Erreur MQ https, tentative de configuration du compte admin")
-                    await self.configurer_admin()
+                    if reponse.status == 200:
+                        if self.__entretien_initial_complete is False:
+                            await self.entretien_initial()
+                        pass  # OK
+                    elif reponse.status == 401:
+                        self.__logger.warning("Erreur MQ https, tentative de configuration du compte admin")
+                        await self.configurer_admin()
+                        await self.entretien_initial()
+                    elif reponse.status == 503:
+                        self.__logger.warning("Erreur MQ https, healthcheck echec")
+                except ClientConnectorError:
+                    self.__logger.exception("MQ n'est pas accessible")
 
         except Exception as e:
             self.__logger.exception("Erreur verification RabbitMQ https")
@@ -46,11 +59,25 @@ class EntretienRabbitMq:
         self.__logger.debug("entretien fin")
 
     async def configurer_admin(self):
+        with open(self.__etat_instance.configuration.instance_password_mq_path, 'r') as fichier:
+            password_mq = fichier.read().strip()
+
         basic_auth = aiohttp.BasicAuth('guest', 'guest')
         async with aiohttp.ClientSession(auth=basic_auth) as session:
-            async with session.get('https://127.0.0.1:8443/api/health/checks/alarms', ssl=self.__sslcontext) as reponse:
-                pass
-        self.__logger.debug("Reponse MQ config admin : %s" % reponse)
+            data = {
+                'tags': 'administrator',
+                'password': password_mq,
+            }
+
+            async with session.put('https://127.0.0.1:8443/api/users/admin', json=data, ssl=self.__sslcontext) as response:
+                self.__logger.debug("Reponse creation admin : %s" % response)
+
+    async def entretien_initial(self):
+        async with self.__session.delete('https://127.0.0.1:8443/api/users/guest', ssl=self.__sslcontext) as response:
+            self.__logger.debug("Reponse suppression guest : %s" % response)
+
+        self.__entretien_initial_complete = True
 
     async def ajouter_compte(self):
         raise NotImplementedError('todo')
+
