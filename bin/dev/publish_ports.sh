510cbd79444809aeb5bc55b29b7c747fd0e0fe4f
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-05-29 17:24:06 -0400

    Fix config coupdoeil

--- bin/dev/publish_ports.sh
+++ bin/dev/publish_ports.sh
@@ -4,8 +4,8 @@
 # docker service update -d --publish-add published=8443,target=8443,mode=host mq
 docker service update -d --publish-add 6379:6379 redis
 docker service update -d --publish-add 27017:27017 mongo
-docker service update -d --publish-add 3001:443 maitrecomptes_web
-docker service update -d --publish-add 3003:443 web_coupdoeil
+docker service update -d --publish-add 3001:443 maitrecomptes
+docker service update -d --publish-add 3003:443 coupdoeil
 docker service update -d --publish-add published=3021,target=443,mode=host fichiers
 docker service update -d --publish-add published=9200,target=9200,mode=host elasticsearch
 docker service update -d --publish-add published=3037,target=443,mode=host collections
