9beb01c26188396cbbaf921a800fb3e0a9a9afa5
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-07-02 08:07:55 -0400

    Maj script restaurer
    Pas inclure parametres directement

--- bin/restaurer.sh
+++ bin/restaurer.sh
@@ -7,4 +7,6 @@ export CERT_PEM=/var/opt/millegrilles/secrets/pki.instance.cert
 export KEY_PEM=/var/opt/millegrilles/secrets/pki.instance.key
 export MQ_HOSTNAME=localhost
 
-python3 -m millegrilles_messages.backup --verbose restaurer --transactions --rechiffrer $@
+# python3 -m millegrilles_messages.backup --verbose restaurer --transactions --rechiffrer $@
+# python3 -m millegrilles_messages.backup --verbose restaurer --workpath ~/tmp --archive ~/tmp/redmine_mariadb.20220702T114639Z.tar
+python3 -m millegrilles_messages.backup $@
