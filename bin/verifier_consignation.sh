33ae26f9a779a412a0410f7cd7656c0be5f47571
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-07-03 15:29:07 -0400

    Ajout script verifier_consignation.sh

---
+++ bin/verifier_consignation.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+source /var/opt/millegrilles/venv/bin/activate
+export PYTHONPATH="${PYTHONPATH}:/var/opt/millegrilles/python"
+
+#export CA_PEM=/var/opt/millegrilles/configuration/pki.millegrille.cert
+#export CERT_PEM=/var/opt/millegrilles/secrets/pki.instance.cert
+#export KEY_PEM=/var/opt/millegrilles/secrets/pki.instance.key
+#export MQ_HOSTNAME=localhost
+
+python3 -m millegrilles_messages.backup --verbose verifier --repertoire
