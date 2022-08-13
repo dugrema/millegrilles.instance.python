e2d7a14c3d826720db443b3f4064318ab1a8647c
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-08-13 14:48:39 -0400

    Fix favicon dans app installation

--- web/src/manifest.build.js
+++ web/src/manifest.build.js
@@ -1,5 +1,5 @@
 const build = {
-  date: '2022-08-08 18:39',
+  date: '2022-08-13 14:44',
   version: '2022.5.0'
 }
 module.exports = build;
--- web/src/manifest.build.js.cp
+++ web/src/manifest.build.js.cp
@@ -1,5 +1,5 @@
 const build = {
-  date: '2022-08-08 13:57',
+  date: '2022-08-08 18:39',
   version: '2022.5.0'
 }
 module.exports = build;
