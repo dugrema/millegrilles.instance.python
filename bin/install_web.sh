6d35952651a1eadb3f7ad80edca48f73c066d567
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-08-13 14:50:54 -0400

    Ajout copie favicon.ico vers nginx/html
    Utilise par navigateurs globalement

--- bin/install_web.sh
+++ bin/install_web.sh
@@ -4,8 +4,10 @@ set -e
 
 REP_SRC=./dist/web
 REP_INSTALLATION=/var/opt/millegrilles/dist/installation
+REP_NGINX_HTML=/var/opt/millegrilles/nginx/html
 
 mkdir -p $REP_INSTALLATION
 
 #./build_src.sh
 cp -r $REP_SRC/* $REP_INSTALLATION
+cp $REP_SRC/favicon.ico $REP_NGINX_HTML
