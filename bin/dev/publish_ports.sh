19157810a5ca53aa16694da4052614413e66d312
mathieu <mathieu.dugre@mdugre.info>
2023-09-20 12:48:49 -0400

    config midcompte avec user:group 984:980

--- bin/dev/publish_ports.sh
+++ bin/dev/publish_ports.sh
@@ -6,12 +6,10 @@ docker service update -d --publish-add 6379:6379 redis
 docker service update -d --publish-add 27017:27017 mongo
 docker service update -d --publish-add 3001:443 maitrecomptes
 docker service update -d --publish-add 3003:443 coupdoeil
-docker service update -d --publish-add published=3021,target=443,mode=host fichiers
+docker service update -d --publish-add published=3021,target=1443,mode=host fichiers
 docker service update -d --publish-add published=9200,target=9200,mode=host elasticsearch
 docker service update -d --publish-add published=3037,target=443,mode=host collections
 docker service update -d --publish-add published=3013,target=443,mode=host senseurspassifs_web
 docker service update -d --publish-add published=3039,target=443,mode=host messagerie_web
-docker service update -d --publish-add published=3029,target=443,mode=host media
-docker service update -d --publish-add published=3030,target=443,mode=host media
 docker service update -d --publish-add 10443:443 mongoexpress
 docker service update -d --publish-add 8983:8983 solr
