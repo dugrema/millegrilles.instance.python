0f86b84ff6f717ff21bcbd14d471a59d4396991f
mathieu <mathieu.dugre@mdugre.info>
2024-06-28 14:33:58 -0400

    Ajout param rep pour verification

--- bin/verifier_consignation.sh
+++ bin/verifier_consignation.sh
@@ -7,6 +7,10 @@ export PYTHONPATH="${PYTHONPATH}:/var/opt/millegrilles/python"
 #export KEY_PEM=/var/opt/millegrilles/secrets/pki.instance.key
 #export MQ_HOSTNAME=localhost
 
-PATH_CONSIGNATION=/var/lib/docker/volumes/millegrilles-consignation/_data/local
+if [ -z "$1" ]; then
+  PATH_CONSIGNATION=/var/lib/docker/volumes/millegrilles-consignation/_data/local
+else
+  PATH_CONSIGNATION=$1
+fi
 
 python3 -m millegrilles_messages.backup --verbose verifier --repertoire $PATH_CONSIGNATION
