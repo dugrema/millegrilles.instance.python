7f47fbfe098448539a701386a9df434f375b752a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-08-23 10:17:13 -0400

    Added private/protected web api exports

--- bin/dev/publish_ports.sh
+++ bin/dev/publish_ports.sh
@@ -4,8 +4,9 @@
 # docker service update -d --publish-add published=8443,target=8443,mode=host mq
 docker service update -d --publish-add 6379:6379 redis
 docker service update -d --publish-add 27017:27017 mongo
-docker service update -d --publish-add 3001:443 maitrecomptes
 docker service update -d --publish-add 3003:443 coupdoeil
+docker service update -d --publish-add published=3001,target=1443,mode=host protected_webapi
+docker service update -d --publish-add published=3001,target=1443,mode=host private_webapi
 docker service update -d --publish-add published=3021,target=1443,mode=host fichiers
 docker service update -d --publish-add published=9200,target=9200,mode=host elasticsearch
 docker service update -d --publish-add published=3037,target=1443,mode=host collections
