2b5aa5f04f80cf655c69fd61c4623d988a6f9186
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-07-27 06:57:30 -0400

    Removed old web code (web monitor)
    Replaced by Coup D'Oeil.

--- web/src/manifest.build.js
+++
@@ -1,5 +0,0 @@
-const build = {
-  date: '2023-07-03 13:03',
-  version: '2023.7.0'
-}
-module.exports = build;
--- web/src/manifest.build.js.cp
+++
@@ -1,5 +0,0 @@
-const build = {
-  date: '2022-11-13 13:16',
-  version: '2022.8.2'
-}
-module.exports = build;
