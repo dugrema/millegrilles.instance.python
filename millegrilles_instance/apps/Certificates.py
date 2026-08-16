cd6b3c450fb2af273c4a2e1645fa4ff811c5c5f6
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-08-16 11:07:34 -0400

    Increase timeout for cert check (rpi 1)

--- millegrilles_instance/apps/Certificates.py
+++ millegrilles_instance/apps/Certificates.py
@@ -232,7 +232,7 @@ async def signer_module_core(producer: MilleGrillesPikaMessageProducer, context:
 async def check_certissuer_available(context: InstanceContext):
     config = context.configuration
     url_issuer = f"{config.certissuer_url}/certificate.pem"
-    timeout = ClientTimeout(3)
+    timeout = ClientTimeout(20)
     if url_issuer.startswith('https'):
         connector = TCPConnector(ssl=context.ssl_context)
         session = ClientSession(timeout=timeout, connector=connector)
