ec231c3385d4a5a689398d8c1faf584b04387804
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-09-09 17:50:50 -0400

    Added self_renew_protege.sh script to renew local manager

---
+++ bin/x509/self_renew_protege.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+# Use this file to renew a 3.protege manager (this includes managers for the 4.secure environment)
+
+if [ -z "$INSTANCE_ID" ]; then
+  echo "Load the environment first"
+fi
+
+"${MILLEGRILLES_ROOT}"/bin/x509/sign_protege.py \
+  --millegrilles-root "${MILLEGRILLES_ROOT}" \
+  --ca-pem "${MILLEGRILLES_ROOT}/secrets/certissuer/signing_ca.pem" \
+  --instance-id $INSTANCE_ID
