962f6680d26ad3dab3308da2c6454b1612918c3a
Mathieu Dugre <mathieu.dugre@mdugre.info>
2026-09-26 09:59:51 -0400

    Added restore_documents.sh script

---
+++ bin/backup/restore_documents.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+if [ -z "$MILLEGRILLES_ROOT" ]; then
+  echo "The environment has to be activated (MILLEGRILLES_ROOT is empty)"
+  exit 1
+fi
+
+CA_KEY_PATH="${MILLEGRILLES_ROOT}/var/backup/domains/ca.pem"
+# Ensure the ca.pem key is present
+if [ ! -f "$CA_KEY_PATH" ]; then
+  echo "The ca.pem key must be placed under $CA_KEY_PATH"
+  exit 1
+fi
+
+APP_YAML="${MILLEGRILLES_ROOT}/etc/compose/applications.yml"
+
+docker compose -f "$APP_YAML" down documents
+docker compose -f "$APP_YAML" run --rm documents ./millegrilles_documents --restore --capath /var/opt/millegrilles/archives/ca.pem
+docker compose -f "$APP_YAML" up -d --remove-orphans documents
